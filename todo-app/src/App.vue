e503dd325cdfbec288d599dfcabb82fe1ae7c5d4
carlosb <mcflurry0@gmail.com>
2017-12-24 21:15:59 +0100

    First skeleton for todo app

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -12,6 +12,31 @@ export default {
   components: {
     TodoList,
   },
+  data()  {
+	  return {
+		  todos:[{
+		  	title: 'Todo A',
+			project: 'Project A',
+			done: false
+		},  {
+		  	title: 'Todo B',
+			project: 'Project B',
+			done: false
+		
+		},{
+		  	title: 'Todo C',
+			project: 'Project C',
+			done: false
+		
+		},{
+		  	title: 'Todo D',
+			project: 'Project D',
+			done: false
+		
+
+		}],
+	  };
+  },
 };
 </script>
 
