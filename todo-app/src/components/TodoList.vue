85aaa17a81f9a17389152cf954bdb0b388cb3f6c
carlosb <mcflurry0@gmail.com>
2017-12-25 23:03:23 +0100

    Added delete form feature

--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -2,7 +2,8 @@
   <div>
   	<p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}} </p>
   	<p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}} </p>
-    
+  
+   <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo"> </todo>
    <todo v-for="todo in todos" v-bind:todo="todo"> </todo>
   
   </div>
@@ -16,6 +17,12 @@ export default {
   props: ['todos'],
   components: {
   	Todo,
+  },
+  methods: {
+  	deleteTodo(todo) {
+		const todoIndex = this.todos.indexOf(todo);
+		this.todos.splice(todoIndex,1);
+	},
   },
 };
 
